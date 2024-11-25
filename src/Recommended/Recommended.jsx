12d93b3530f40eeaca4535ef205cef0f05dd5563
maundu55 <marymaundu.mm@gmail.com>
2024-11-25 15:23:28 +0300

    Add css to the Recommended section

--- src/Recommended/Recommended.jsx
+++ src/Recommended/Recommended.jsx
@@ -4,8 +4,8 @@ import './Recommended.css'
 const Recommended = () => {
   return (
     <div>
-      <h2>Recommended</h2>
-      <div className='recommended-btns'>
+      <h2 className='recommended-title'>Recommended</h2>
+      <div className='recommended-flex'>
       <button className='btns'>All Products</button>
       <button className='btns'>Nike</button>
       <button className='btns'>Adidas</button>
