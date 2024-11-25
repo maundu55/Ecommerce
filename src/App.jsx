b6f5bb81a5f1cdbd8e92ad0f56e99d6f6cb9a7b0
maundu55 <marymaundu.mm@gmail.com>
2024-11-25 13:35:16 +0300

    Ecommerce site using React Js

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+import './index.css'
+import Nav from './Navigation/Nav'
+import Products from './Products/Products'
+import Recommended from "./Recommended/Recommended"
+
+
+function App() {
+  
+
+  return (
+    <>
+    <Nav />
+    <Products />
+    <Recommended />
+  
+    </>
+  )
+}
+
+export default App
