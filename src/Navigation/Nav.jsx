78bf5e92c9c900b85efbfbb72fe292ae60791575
maundu55 <marymaundu.mm@gmail.com>
2024-11-25 13:55:23 +0300

    add react icons

--- src/Navigation/Nav.jsx
+++ src/Navigation/Nav.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { FiHeart } from "react-icons/fi";
+import { AiOutlineShoppingCart, AiOutlineUserAdd } from "react-icons/ai";
 import './Nav.css'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <div className='nav-container'>
+        <input type="text" className='search-input' placeholder='Enter your search shoes' />
+      </div>
+      <div className='profile-container'>
+        <a href="#">
+        <FiHeart className='nav-icons' />
+        </a>
+        <a href="">
+          <AiOutlineShoppingCart className="nav-icons" />
+          <AiOutlineUserAdd className="nav-icons"/>
+        </a>
+      </div>
+    </nav>
   )
 }
 
