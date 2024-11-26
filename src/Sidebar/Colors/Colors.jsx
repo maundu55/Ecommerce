d9a12695d49250a74b5a278f04ed3e1226234854
maundu55 <marymaundu.mm@gmail.com>
2024-11-26 11:04:37 +0300

    Add color component

--- src/Sidebar/Colors/Colors.jsx
+++ src/Sidebar/Colors/Colors.jsx
@@ -3,7 +3,33 @@ import './Colors.css'
 
 const Colors = () => {
   return (
-    <div>Colors</div>
+    <div>
+      <h2 className='sidebar-title price-title'>Colors</h2>
+        <label className='sidebar-label-container'>
+          <input type="radio" name='test2' />
+          <span className='checkmark'></span>All
+        </label>
+      <label className='sidebar-label-container'>
+          <input type="radio" name='test2' />
+          <span className='checkmark'></span>Black
+        </label>
+      <label className='sidebar-label-container'>
+          <input type="radio" name='test2' />
+          <span className='checkmark'></span>Blue
+        </label>
+      <label className='sidebar-label-container'>
+          <input type="radio" name='test2' />
+          <span className='checkmark'></span>Red
+        </label>
+      <label className='sidebar-label-container'>
+          <input type="radio" name='test2' />
+          <span className='checkmark'></span>Green
+        </label>
+      <label className='sidebar-label-container'>
+          <input type="radio" name='test2' />
+          <span className='checkmark'></span>White
+        </label>
+    </div>
   )
 }
 
